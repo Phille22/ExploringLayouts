31779087f3297acb29169c6c8ec1101b9e975207
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-04-26 13:12:28 +0200

    Views, knappar, text och rating bar är tillagda

--- app/src/main/java/com/example/exploringlayouts/MainActivity.java
+++ app/src/main/java/com/example/exploringlayouts/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.exploringlayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
     }
+
+    void loadConstraintLayout(View view){
+        setContentView(R.layout.activity_main);
+    }
+
+    //void loadTableLayout(View view){
+    //    setContentView(R.layout.my_table_layout);
+    //}
 }
